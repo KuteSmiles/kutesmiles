b4d93845abe51f7b807057cf54bcc654e47309a1
KuteSmiles <150293213+KuteSmiles@users.noreply.github.com>
2023-11-08 14:45:54 -0600

    Update index.js

--- index.js
+++ index.js
@@ -10,4 +10,16 @@
     -webkit-overflow: hidden;
   `;
   document.body.appendChild(fulldisplay);
+
+  var topbar = document.createElement("div");
+  topbar.style = `
+    position: absolute;
+    left: 0%;
+    top: 0%;
+    width: 100%;
+    height: calc((0.4cm) + (18pt));
+    overflow: hidden;
+    border-bottom: solid 1px #FFFFA3;
+  `;
+  fulldisplay.appendChild(topbar);
 }).call({});
